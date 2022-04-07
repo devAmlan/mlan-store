71cc0506df7d6d27d2d636f88cb94db7e2378bfb
Aj-github-cmd <sahooamlanjyoti99@gmail.com>
2022-04-07 10:51:07 +0530

    deleted commented codes

--- frontend/src/reducer/FilterReducer.js
+++ frontend/src/reducer/FilterReducer.js
@@ -1,29 +1,20 @@
 
 export function FilterReducer(state, action) {
-  //  const noFilter = {
-  //   sortBy:null,
-  //   rating:null,
-  //   category:{
-  //     keyboard:false,
-  //     switch:false,
-  //     others:false
-  //   }
-  //  }
-   const {keyboards,switches,others} = state.category
-  switch(action.type){
-   case "sortBy":
-    // console.log(action.payload)
-    return {...state,sortBy:action.payload}
-   case "rating":
-    return {...state,rating:parseInt(action.payload)}
-    
-   case "keyboards":
-    return {...state,category:{...state.category,keyboards:!keyboards}}
-   case "switches":
-     return {...state,category:{...state.category,switches:!switches}}
-   case "others":
-     return {...state,category:{...state.category,others:!others}}
-   default :
-   return {...state}
+
+  const { keyboards, switches, others } = state.category
+  switch (action.type) {
+    case "sortBy":
+      return { ...state, sortBy: action.payload }
+    case "rating":
+      return { ...state, rating: parseInt(action.payload) }
+
+    case "keyboards":
+      return { ...state, category: { ...state.category, keyboards: !keyboards } }
+    case "switches":
+      return { ...state, category: { ...state.category, switches: !switches } }
+    case "others":
+      return { ...state, category: { ...state.category, others: !others } }
+    default:
+      return { ...state }
   }
 } 
